2c0c0024a54e74970a0d50857717f4226a78ad17
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2019-10-11 15:10:12 -0400

    updated code to show results from all ships

--- src/App.js
+++ src/App.js
@@ -1,38 +1,68 @@
+/********
+ * 1- Get Starwars ships information from Swapi api - DONE
+ * 2- Display in Dom - DONE
+ * 3- Store images on S3 bucket - 
+ * 4- Get images from S3 bucket 
+ * 5- Match the name of the Ship with Image name
+ * 6- Display on Dom each image with its information
+ */
+
+
 import React from "react";
 import "./App.css";
+
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      loading: true,
-      shipInfo: {}
+      apiData: [],
+      loading: true
     };
   }
 
   componentDidMount() {
-    fetch("https://swapi.co/api/starships/9/")
+    fetch("https://swapi.co/api/starships/")
       .then(response => response.json())
       .then(data => {
+        //settin state with results array
         this.setState({
-          shipInfo: data,
-          loading: false
+          apiData: data.results,
+          loading: false,
+
         });
       });
+
+    fetch("http://www.splashbase.co/api/v1/images/random")
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data)
+      })
   }
 
   render() {
-    console.log(this.state.shipInfo);
-    const shipName = this.state.loading
-      ? "loading...."
-      : this.state.shipInfo.name;
+    const allShips = this.state.apiData;
+    const isLoading = this.state.loading;
+
+    console.log(allShips);
+
+    const newShipsArray = allShips.map(function(item, index) {
+      if(!isLoading){
+        return (
+        <div>className=''> 
+          <h3>{item.name}</h3>
+          <p className='text-muted'>{item.model}</p>
+        </div> 
+        )
+      } return 'loading....'
+    });
 
     return (
       <div className="main-wrapper">
-        <div className="main-container">
+        <div className="main-container container">
           <h1>Starships</h1>
-          <p>{shipName}</p>
-          <p>{this.state.shipInfo.model}</p>
+          {newShipsArray}
+          <img width="200px" src="https://starwars-ships.s3.amazonaws.com/starwars-1.png" alt="Starwars here"/> 
         </div>
       </div>
     );
