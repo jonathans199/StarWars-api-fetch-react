d742e411df78099c6a813f57a04a139b5082ef4d
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2019-10-10 22:50:35 -0400

    updated with image and working FETCH with api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from "react";
+import "./App.css";
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      loading: true,
+      shipInfo: {}
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://swapi.co/api/starships/9/")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          shipInfo: data,
+          loading: false
+        });
+      });
   }
 
   render() {
-    return <div>clean slate here</div>;
+    console.log(this.state.shipInfo);
+    const shipName = this.state.loading
+      ? "loading...."
+      : this.state.shipInfo.name;
+
+    return (
+      <div className="main-wrapper">
+        <div className="main-container">
+          <h1>Starships</h1>
+          <p>{shipName}</p>
+          <p>{this.state.shipInfo.model}</p>
+        </div>
+      </div>
+    );
   }
 }
 
