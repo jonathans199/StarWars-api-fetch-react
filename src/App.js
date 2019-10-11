60ba0c387643215cac0a300b5d975b6f0965c41e
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2019-10-11 16:42:09 -0400

    updated images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 /********
  * 1- Get Starwars ships information from Swapi api - DONE
  * 2- Display in Dom - DONE
- * 3- Store images on S3 bucket - 
- * 4- Get images from S3 bucket 
- * 5- Match the name of the Ship with Image name
- * 6- Display on Dom each image with its information
+ * 3- Store images on S3 bucket - DONE
+ * 4- Get images from S3 bucket - DONE
+ * 5- Match the name of the Ship with Image name - not needed
+ * 6- Display on Dom each image with its information - 
  */
 
 
@@ -17,7 +17,19 @@ class App extends React.Component {
     super();
     this.state = {
       apiData: [],
-      loading: true
+      loading: true,
+      images: [
+        {imageUrl: "https://starwars-ships.s3.amazonaws.com/starwars-1.png"},
+        {imageUrl: "https://starwars-ships.s3.amazonaws.com/starwars-2.png"},
+        {imageUrl: "https://starwars-ships.s3.amazonaws.com/starwars-3.png"},
+        {imageUrl: "https://starwars-ships.s3.amazonaws.com/starwars-4.png"},
+        {imageUrl: "https://starwars-ships.s3.amazonaws.com/starwars-5.png"},
+        {imageUrl: "https://starwars-ships.s3.amazonaws.com/starwars-2.png"},
+        {imageUrl: "https://starwars-ships.s3.amazonaws.com/starwars-7.png"},
+        {imageUrl: "https://starwars-ships.s3.amazonaws.com/starwars-3.png"},
+        {imageUrl: "https://starwars-ships.s3.amazonaws.com/starwars-9.png"},
+        { imageUrl: "https://starwars-ships.s3.amazonaws.com/starwars-10.png"}
+      ]
     };
   }
 
@@ -25,44 +37,51 @@ class App extends React.Component {
     fetch("https://swapi.co/api/starships/")
       .then(response => response.json())
       .then(data => {
+
+
         //settin state with results array
         this.setState({
           apiData: data.results,
           loading: false,
-
+  
         });
       });
-
-    fetch("http://www.splashbase.co/api/v1/images/random")
-      .then(resp => resp.json())
-      .then(data => {
-        console.log(data)
-      })
   }
 
   render() {
     const allShips = this.state.apiData;
     const isLoading = this.state.loading;
+    const allImages = this.state.images;
 
-    console.log(allShips);
+    
+    // add image URL to the array 
+    console.log(allShips)
+    allShips.forEach(element => {
+      element.imageUrl = `https://starwars-ships.s3.amazonaws.com/starwars-${Math.floor(
+        Math.random() * 10
+      )}.png`;
+    });
 
     const newShipsArray = allShips.map(function(item, index) {
+
       if(!isLoading){
         return (
-        <div>className=''> 
-          <h3>{item.name}</h3>
-          <p className='text-muted'>{item.model}</p>
-        </div> 
-        )
+          <div>
+            <h3>{item.name}</h3>
+            <p className="text-muted">{item.model}</p>
+            <img src={item.imageUrl} width="200px" />
+          </div>
+        );
       } return 'loading....'
     });
+
 
     return (
       <div className="main-wrapper">
         <div className="main-container container">
           <h1>Starships</h1>
           {newShipsArray}
-          <img width="200px" src="https://starwars-ships.s3.amazonaws.com/starwars-1.png" alt="Starwars here"/> 
+          
         </div>
       </div>
     );
